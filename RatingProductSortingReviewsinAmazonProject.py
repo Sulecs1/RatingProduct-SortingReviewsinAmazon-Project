bea9d4ce0fe3308b52b41a5096ed8ade68e7bff8
Şule Akçay <61309605+Sulecs1@users.noreply.github.com>
2021-02-14 21:22:45 +0300

    Update RatingProductSortingReviewsinAmazonProject.py

--- RatingProductSortingReviewsinAmazonProject.py
+++ RatingProductSortingReviewsinAmazonProject.py
@@ -20,16 +20,6 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
 
-##############Features#################
-# - Kullanıcı ID’si. Örn: A2SUAM1J3GNN3B
-#asin – Ürün ID’si. Örn: 0000013714
-#reviewerName – Kullanıcı Adı
-#helpful – Faydalı yotum derecesi. Örn:. 2/3
-#reviewText – Kullanıcın yazdığı inceleme metni
-#overall – Ürün rating’i
-#summary - İnceleme özeti
-#unixReviewTime – İnceleme zamanı (unix time)
-#reviewTime – İnceleme zamanı (raw)
 
 # credentials.
 creds = {'user': 'group4',
